7a4eae5bb53c5f7e051714b0e2d714da223f2009
Dylan Stratton <dm.stratton94@gmail.com>
2018-03-20 16:09:47 -0500

    improve search functionality

--- routes/notes.js
+++ routes/notes.js
@@ -10,19 +10,50 @@ const Note = require('../models/note');
 
 /* ========== GET/READ ALL ITEM ========== */
 router.get('/notes', (req, res, next) => {
+  // mongoose
+  //   .connect(MONGODB_URI)
+  //   .then(() => {
+  //     const searchTerm = req.query.searchTerm;
+  //     let filter = {};
+
+  //     if (searchTerm) {
+  //       const re = new RegExp(searchTerm, 'i');
+  //       filter.title = { $regex: re };
+  //     }
+
+  //     return Note.find(filter)
+  //       .sort('created')
+  //       .then(results => {
+  //         res.json(results);
+  //       })
+  //       .catch(next);
+  //   })
+  //   .then(() => {
+  //     return mongoose.disconnect().then(() => {
+  //       console.info('Disconnected');
+  //     });
+  //   })
+  //   .catch(err => {
+  //     console.error(`ERROR: ${err.message}`);
+  //     console.error(err);
+  //   });
+
   mongoose
     .connect(MONGODB_URI)
+    .then(() => Note.createIndexes())
     .then(() => {
       const searchTerm = req.query.searchTerm;
+
       let filter = {};
 
       if (searchTerm) {
-        const re = new RegExp(searchTerm, 'i');
-        filter.title = { $regex: re };
+        filter.$text = { $search: searchTerm };
       }
 
-      return Note.find(filter)
-        .sort('created')
+      return Note.find(
+        filter,
+        { score: { $meta: 'textScore' } }
+      ).sort({ score: { $meta: 'textScore' } })
         .then(results => {
           res.json(results);
         })
@@ -37,6 +68,8 @@ router.get('/notes', (req, res, next) => {
       console.error(`ERROR: ${err.message}`);
       console.error(err);
     });
+
+
 });
 
 /* ========== GET/READ A SINGLE ITEM ========== */
